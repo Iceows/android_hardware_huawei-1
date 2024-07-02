sprd-oss-devs/android_hardware_sprd

light: Refactor
Change-Id: Ib4ae8e9821dd0fb36e922e2c5f993aa6c0e28be1

light/service.cpp
@@ -1,10 +1,12 @@
 /*
- * Copyright (C) 2021 The LineageOS Project
+ * Copyright (C) 2021-2024 The LineageOS Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include "Lights.h"
+
+#define LOG_TAG "android.hardware.light-service.sprd"
 
 #include <android-base/logging.h>
 #include <android/binder_manager.h>

Iceows/android_hardware_huawei-1

light/service.cpp
@@ -1,10 +1,12 @@
 /*
- * Copyright (C) 2021 The LineageOS Project
+ * Copyright (C) 2021-2024 The LineageOS Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include "Lights.h"
+
+#define LOG_TAG "android.hardware.light-service.huawei"
 
 #include <android-base/logging.h>
 #include <android/binder_manager.h>